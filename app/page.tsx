f4828b7b1480570719dd9b258c3a42111a115683
Shery <chsheharyarahmed@gmail.com>
2024-05-07 22:08:56 +0500

    portfolio section added

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ function HeaderButton({ children }: { children: React.ReactNode }) {
 
 function Header() {
   return (
-    <div className="h-8 flex flex-row text-gray-50 py-6 w-full gap-x-4 justify-end items-center">
+    <div className="h-8 flex flex-row text-gray-50 py-6 w-full gap-x-4 justify-end items-center font-sans">
       <HeaderButton>Github</HeaderButton>
       <HeaderButton>Resume</HeaderButton>
     </div>
@@ -21,17 +21,39 @@ function Hero() {
       <h1 className="text-6xl font-serif">Sheharyar Ahmed</h1>
       <p className="font-sans text-md">Software Developer</p>
       <p className="font-sans text-xl mt-2">
-        Building modern digital experiences es on Mobile and Web.
+        Building modern digital experiences on Mobile and Web.
       </p>
+    </div>
+  );
+}
+
+function PortfolioCard() {
+  return (
+    <div className="mt-4 ml-4 mb-8  max-w-screen-md font-sans">
+      <h3 className="text-xl">Contactless Checkout for Android</h3>
+      <p className="text-gray-300 text-xs">Huma Production</p>
+      <p className="mt-2">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+    </div>
+  );
+}
+
+function Portfolio() {
+  return (
+    <div className="mt-8 text-gray-50">
+      <h2 className="font-serif text-4xl">Portfolio</h2>
+      {Array.from({ length: 4 }).map((_, index) => (
+        <PortfolioCard key={index} />
+      ))}
     </div>
   );
 }
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="px-4">
       <Header />
       <Hero />
+      <Portfolio />
     </main>
   );
 }
