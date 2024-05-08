fb0a7f701d2d09929a1c2b4e331b8b9c9d9a28a2
Shery <chsheharyarahmed@gmail.com>
2024-05-08 15:46:31 +0500

    Contactless portfolio added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { forwardRef } from "react";
 
 function HeaderButton({
   children,
@@ -66,45 +67,164 @@ function PortfolioCard({
 }: {
   title: string;
   subTitle: string;
-  tags: Array<Parameters<typeof Tag>[0]>;
-  description: React.ReactNode;
+  tags?: Array<Parameters<typeof Tag>[0]>;
+  description: JSX.Element;
 }) {
   return (
     <div className="mt-4 ml-0 mb-8  max-w-screen-md font-sans">
       <div className="flex gap-y-1 flex-wrap flex-row gap-x-3 justify-start items-center">
         <h3 className="h3">{title}</h3>
-        <div className="flex flex-row gap-y-1 justify-start items-center flex-wrap gap-x-2">
-          {tags.map((i, index) => (
-            <Tag key={index} icon={i.icon} href={i.href}>
-              {i.children}
-            </Tag>
-          ))}
-        </div>
+        {tags && tags.length > 0 ? (
+          <div className="flex flex-row gap-y-1 justify-start items-center flex-wrap gap-x-2">
+            {tags.map((i, index) => (
+              <Tag key={index} icon={i.icon} href={i.href}>
+                {i.children}
+              </Tag>
+            ))}
+          </div>
+        ) : null}
       </div>
       <p className="text-gray-300 mt-1 text-xs">{subTitle}</p>
-      <div className="mt-2 text-gray-300">{description}</div>
+      <div className="mt-2 text-sm text-gray-300">{description}</div>
     </div>
   );
 }
+
+// { children: "Android" },
+// { children: "Live", href: "https://google.com" },
+// {
+//   children: "Source Code",
+//   href: "https://github.com/SheryConcepts",
+// },
+
+const InlineLink = forwardRef<
+  HTMLAnchorElement,
+  React.ComponentPropsWithRef<typeof Link>
+>(({ children, ...props }, ref) => {
+  return (
+    <Link
+      {...props}
+      className="text-white underline-offset-3 cursor-pointer underline"
+      href={props.href}
+      ref={ref}
+      target={props.target ? props.target : "_blank"}
+    >
+      {children}
+    </Link>
+  );
+});
+InlineLink.displayName = "InlineLink";
+
+const Portfolios: Array<Parameters<typeof PortfolioCard>[0]> = [
+  {
+    subTitle: "Huma Productions",
+    title: "Contactless Checkout",
+    tags: [
+      {
+        children: "iOS",
+      },
+      { children: "Mobile" },
+      {
+        children: "Live",
+        href: "https://apps.apple.com/us/app/huma-contactless/id6478042324",
+      },
+    ],
+    description: (
+      <div>
+        <p>
+          Huma Contactless for iOS is a hassle-free self checkout app. It helps
+          Shoppers find, scan, pay (self checkout) and manage their groceries. I
+          was the hired as a solo developer by Huma Productions to turn their
+          idea into reality.
+        </p>
+        <div className="ml-4">
+          <li>
+            <InlineLink href="https://reactnative.dev">React Native</InlineLink>{" "}
+            was used as our Framework of choice, with{" "}
+            <InlineLink href={"https://www.typescriptlang.org"}>
+              Typescript
+            </InlineLink>{" "}
+            for a typesafe and modular codebase.
+          </li>
+          <li>
+            For styling of our app we decided to use{" "}
+            <InlineLink href="https://tailwindcss.com">Tailwindcss</InlineLink>{" "}
+            enabled by{" "}
+            <InlineLink href="https://nativewind.dev">Nativewind</InlineLink>,
+            rather than simple CSS Stylesheet in React Native.
+          </li>
+          <li>
+            Backend of the app was a bit tricky to implement, because it also
+            involved CMS for Shop Owners to manager their Inventory.{" "}
+            <InlineLink href={"https://www.odoo.com/"}>Odoo</InlineLink> was
+            used for this purpose. Odoo was integrated into our backend with
+            APIs. <InlineLink href={"https://nextjs.org"}>Nextjs</InlineLink>{" "}
+            was used for writing the Backend, deployed on Serverless
+            Infrastructure by{" "}
+            <InlineLink href="https://vercel.com">Vercel</InlineLink>.
+          </li>
+          <li>
+            For Authorization and Authenctication we used{" "}
+            <InlineLink href="https://clerk.com">Clerk</InlineLink>.
+          </li>
+          <li>
+            <InlineLink href="https://stripe.com">Stripe</InlineLink> was used
+            for implementing Payments for our platform.
+          </li>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Contactless Checkout",
+    subTitle: "Huma Productions",
+    tags: [
+      {
+        children: "Android",
+      },
+      { children: "Mobile" },
+      {
+        children: "Live",
+        href: "https://play.google.com/store/apps/details?id=com.huma.contactless",
+      },
+    ],
+    description: (
+      <div className="space-y-2">
+        <p>
+          I was hired again for an Android version of our app. Using React
+          Native paid back back. I implemented a new module for our app, to
+          allow shop keeper to sign as admin and scan products for adminstration
+          puposes.
+        </p>
+        <p>
+          Tasked with making our existing React Native Typescript code base
+          cross-platform for Android and publishing app on Playstore.
+        </p>
+        <p>
+          Most challenging task was making iOS Product Scanner compatible with
+          Android.
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "Headless Eccommerce Storefront",
+    subTitle: "Panaverse",
+    description: <div></div>,
+  },
+];
 
 function Portfolio() {
   return (
     <div className="mt-8 text-gray-50">
       <h2 className="h2">Portfolio</h2>
-      {Array.from({ length: 4 }).map((_, index) => (
+      {Portfolios.map((i, index) => (
         <PortfolioCard
           key={index}
-          description="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
-          subTitle="Huma Productions"
-          tags={[
-            { children: "Live", href: "https://google.com" },
-            { children: "Android" },
-            {
-              children: "Source Code",
-              href: "https://github.com/SheryConcepts",
-            },
-          ]}
-          title="Contactless Checkout"
+          description={i.description}
+          subTitle={i.subTitle}
+          tags={i.tags}
+          title={i.title}
         />
       ))}
     </div>
